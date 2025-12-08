647334600e072de1c81a575b90ca408a4689e73e
krame965 <krame965@umn.edu>
2025-12-08 16:13:57 -0600

    update setup.py

--- src/april_tag_navigator/setup.py
+++ src/april_tag_navigator/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'april_tag_navigator'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +30,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'april_tag_navigator = april_tag_navigator.april_tag_navigator:main'
         ],
     },
 )
