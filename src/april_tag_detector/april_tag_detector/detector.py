2416bb48b3cbd40000a88f8ab2f428833dc532d7
krame965 <krame965@umn.edu>
2025-12-13 14:04:14 -0600

    fixes

--- src/april_tag_detector/april_tag_detector/detector.py
+++ src/april_tag_detector/april_tag_detector/detector.py
@@ -375,7 +375,7 @@ class AprilTagDetector(Node):
             approx = cv2.approxPolyDP(cnt, 0.04 * peri, True)
             area = cv2.contourArea(cnt)
             
-            if area < 400:
+            if area < 1500:
                 cv2.polylines(debug_image, [approx.astype(int)], True, (0, 0, 255), 1)
                 continue
 
